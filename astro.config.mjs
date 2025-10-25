3ee0fc5de26f917f1d21631c27f44d6f57e58273
Subrata Das <98220323+subdas374@users.noreply.github.com>
2025-10-25 20:56:59 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://YOUR-USERNAME.github.io',
+  site: 'https://subdas374.github.io',
   integrations: [tailwind()],
 });
